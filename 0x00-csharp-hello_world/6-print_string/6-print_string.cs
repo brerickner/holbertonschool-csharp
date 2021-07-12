2426ad65aa0e9a9dfbb91e4286ed31d0a95ab16b
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-12 16:04:17 -0500

    seeing if the checker will allow imports other than system

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,12 @@
+﻿  
+using System;
+using System.Text;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = new StringBuilder("Holberton School".Length * 3).Insert(0, "Holberton School", 3).ToString();
+        Console.WriteLine(str + "\n" + str.Substring(0,9));
+        }
+}
