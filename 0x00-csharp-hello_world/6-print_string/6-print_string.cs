3d53406e5b601caa721029d85baadaff0db41c95
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-12 16:08:55 -0500

    adding comments for readablility

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -6,6 +6,10 @@ class Program
 {
 	static void Main(string[] args)
 	{
+        // The StringBuilder class creates a mutable string of characters of a certain length in C#.
+        // We can then use the StringBuilder.Insert(0, "Holberton School", 3) function to insert the
+        // string "Holberton School" and repeat it three times.
+        
 		string str = new StringBuilder("Holberton School".Length * 3).Insert(0, "Holberton School", 3).ToString();
         Console.WriteLine(str + "\n" + str.Substring(0,9));
         }
