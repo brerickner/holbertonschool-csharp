7ed934a367ba4d45e29fe5e3d1acc463f1bb5c02
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 10:44:48 -0500

    adding task 4. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value;
+            int toBase = 16;
+            for (value=0; value < 99; value++)
+            {
+                string hex = Convert.ToString(value, toBase);
+                Console.WriteLine($"{value} = 0x{hex}");
+            };
+        }
+    }
+}
