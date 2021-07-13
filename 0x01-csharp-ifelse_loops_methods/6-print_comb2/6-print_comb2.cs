290ea2d6d2528aa530262c018b2141e1de5a14e0
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 14:23:19 -0500

    adding task 6. output looks correct

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            for (i = 0; i < 10; i++)
+            {
+                for (j = 0; j < 10; j++)
+                {
+                    if (j > i)
+                    {
+                        if (i == 8 & j == 9)
+                            Console.Write($"{i}{j}");
+                        else
+                            Console.Write($"{i}{j}, ");
+                    }
+                }
+            }
+        }
+    }
+}
