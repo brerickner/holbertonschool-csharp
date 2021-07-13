fec9d165e46bd6f2506edaf2bb5fd6fec0f37348
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 10:58:51 -0500

    adding task 5. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i = 0; i < 100; i++)
+            {
+                if (i < 10)
+                    Console.Write($"0{i}, ");
+                else if (i == 99)
+                    Console.Write($"{i}");
+                else
+                    Console.Write($"{i}, ");
+            }
+        }
+    }
+}
