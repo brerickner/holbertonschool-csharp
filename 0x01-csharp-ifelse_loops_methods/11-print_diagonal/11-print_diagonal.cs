1403f0f142b1b7de5a2554874811452e2bbda2e4
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 16:45:52 -0500

    adding task 11. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.WriteLine("\n");
+        int x, y;
+        for (x=0;x<length;x++)
+        {
+            for (y=0;y<x;y++)
+            {
+                Console.Write(" ");
+            }
+            Console.WriteLine("\\"); 
+        }
+    }
+}
