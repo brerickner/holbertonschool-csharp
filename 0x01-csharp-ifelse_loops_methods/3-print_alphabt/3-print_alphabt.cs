acd8417401fb1a7ef2c50b77b81a97a655d20df8
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 10:36:42 -0500

    adding task 3. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char i;
+            for (i='a'; i<='z'; i++)
+                if (i != 'q' & i != 'e')
+                    Console.Write(i);
+        }
+    }
+}
