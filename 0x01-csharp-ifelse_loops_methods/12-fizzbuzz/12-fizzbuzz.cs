1a2b431fc3b172e5b949cb9f00b3a9d852a15249
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 17:10:40 -0500

    adding task 12. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/12-fizzbuzz/12-fizzbuzz.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12_fizzbuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i=1;i<101;i++)
+            {
+                if (i % 5 == 0 & i % 3 == 0)
+                    Console.Write("FizzBuzz ");
+                else if (i % 3 == 0)
+                    Console.Write("Fizz ");
+                else if (i % 5 == 0)
+                {
+                    if (i == 100)
+                        Console.WriteLine("Buzz");
+                    else
+                        Console.Write("Buzz ");
+                }
+                else
+                    Console.Write($"{i} ");
+            }
+        }
+    }
+}
