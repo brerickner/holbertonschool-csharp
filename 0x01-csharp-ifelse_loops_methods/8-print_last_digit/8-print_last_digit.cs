8c59be93bda016844e4da5d6a6442cad7955584c
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 15:55:08 -0500

    adding task 8. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int last_num = number % 10;
+        if (last_num < 0)
+            last_num = last_num * -1;
+            Console.Write(last_num);
+        return(last_num);
+    }
+}
