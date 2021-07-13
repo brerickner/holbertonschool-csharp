1a131bbdc84bf61b23a01a8126133544f1841c74
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-13 16:21:14 -0500

    adding task 10. output looks good

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        int i;
+        if (length <= 0)
+            Console.WriteLine("\n");
+        for (i=0; i<length; i++)
+        {
+            if (i == length)
+                Console.WriteLine("\n");
+            else
+                Console.Write("_");
+        }
+    }
+}
+
