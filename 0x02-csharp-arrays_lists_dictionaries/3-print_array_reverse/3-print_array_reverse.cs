8d5ed51b74ee417aaa1d75b3dfd54b481342c6f9
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 16:09:59 -0500

    adding task 3. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_reverse/3-print_array_reverse.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array == null || array.Length == 0)
+        {
+            Console.WriteLine();
+            return;
+        }
+
+        int i = array.Length - 1;
+        while (i > -1)
+        {
+            Console.Write(array[i]);
+            if (i > 0)
+            {
+                Console.Write(" ");
+            }
+            i--;
+        }
+    }
+}
