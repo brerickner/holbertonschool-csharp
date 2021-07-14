a836015e0e38755eebafa3808a6f713b4e8599f2
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 14:51:30 -0500

    adding task 0. output looks good.

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        int[] num_array = new int[size];
+        if (size == 0)
+        {
+            Console.WriteLine();
+            return (num_array);
+        }
+        int i = 0;
+        while (i < num_array.Length)
+        {
+            num_array[i] = i;
+            Console.Write(num_array[i]);
+            if (i < num_array.Length - 1)
+                Console.Write(" ");
+            i++;
+        }
+        Console.WriteLine();
+        return (num_array);
+    }
+}
