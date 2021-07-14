2cfe7a13bb6c64bc260f6d0ce6ea2110082535cd
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 18:12:08 -0500

    adding task 6. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        if (myList == null)
+            return (null);
+        List<bool> listy = new List<bool>();
+        foreach  (int thing in myList)
+        {
+            if (thing % 2 == 0)
+                listy.Add(true);
+            else
+                listy.Add(false);
+        }
+        return (listy);
+    }
+}
+
