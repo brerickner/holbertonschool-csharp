eea54cb3658eb1f7dcd0e0073222cd53bf550bf8
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 16:48:08 -0500

    adding task 4. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        List<int> listy = new List<int>();
+        for (int i = 0; i < size; i++)
+        {
+            listy.Add(i);
+            Console.Write(listy[i]);
+
+            if (i < size - 1)
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+        return (listy);
+    }
+}
