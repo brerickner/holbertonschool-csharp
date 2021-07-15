ab90257a3b6c3a47b59a3ea3c47f8e499cf3af0d
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 11:44:00 -0500

    adding task 0. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        HashSet<int> hashy = new HashSet<int>(myList);
+        int sum = 0;
+        
+        foreach (int i in hashy)
+            sum += i;  
+        return (sum);
+    }
+}
