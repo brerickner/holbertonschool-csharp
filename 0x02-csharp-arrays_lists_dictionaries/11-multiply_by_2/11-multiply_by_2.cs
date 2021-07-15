3188758a1cc216a604f51ce5a05c0a2d05bd8e91
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 21:49:56 -0500

    adding task 11. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        Dictionary<string, int> legitDict = new Dictionary<string, int>();
+        foreach (var val in myDict)
+        {
+            legitDict.Add(val.Key, val.Value * 2);
+        }
+        return (myDict);
+    }
+}
