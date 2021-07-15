650b6d15b86db8a9182d06169cf5f6b1a9d574c1
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 12:09:54 -0500

    adding task 1. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        if (list1 == null || list2 == null)
+            return (null);
+
+        HashSet<int> set1 = new HashSet<int>(list1);
+        HashSet<int> set2 = new HashSet<int>(list2);
+
+     // Creating a new HashSet that contains
+        // the Intersection of both the HashSet mySet1 & mySet2
+        HashSet<int> hashy = new HashSet<int>(set1);
+        
+        hashy.IntersectWith(set2);
+        List<int> newList = new List<int>(hashy);
+        return (newList);
+    }
+}
