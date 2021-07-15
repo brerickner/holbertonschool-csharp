38dd1cd3502eb7073cc8b479b5c75cf2ffe4860c
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 23:00:57 -0500

    adding task 13. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        string freshKey = "";
+        int highScore = 0;
+        foreach (KeyValuePair<string, int> newScore in myList)
+        {
+            if (newScore.Value > highScore)
+            {
+                highScore = newScore.Value;
+                freshKey = newScore.Key;
+            }
+        }
+        return (freshKey);
+    }
+}
