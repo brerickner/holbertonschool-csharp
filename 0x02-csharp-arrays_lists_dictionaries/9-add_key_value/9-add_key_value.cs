0409121c744cf8f5fa821f09eb2339b52285c56f
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 21:22:46 -0500

    adding task 9. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/9-add_key_value/9-add_key_value.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+class Dictionary
+{
+    public static Dictionary<string, string> AddKeyValue(Dictionary<string, string> myDict, string key, string value)
+    {
+        if (myDict == null)
+            return (null);
+        if (myDict.ContainsKey(key))
+            myDict.Add(key, myDict[key]);
+        else
+            myDict.Add(key, value);
+
+        return(myDict);
+    }
+}
