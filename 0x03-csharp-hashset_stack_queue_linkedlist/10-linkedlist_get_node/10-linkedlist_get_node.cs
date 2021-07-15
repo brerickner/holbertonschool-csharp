31afa0efe376f6d64ccf9a0a50f5dee578e4e5cf
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 18:54:09 -0500

    adding task 10. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        var hold = 0;
+        foreach(int val in myLList)
+        {
+            if (hold == n)
+                return (val);
+            hold++;
+        }
+        return (0);
+    }
+}
