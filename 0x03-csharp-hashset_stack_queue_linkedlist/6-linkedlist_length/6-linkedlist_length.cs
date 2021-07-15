06db2f1d360bf217be4c1294ffd6731310a91982
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 17:38:30 -0500

    addign task 6. output good. we shall see

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/6-linkedlist_length/6-linkedlist_length.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Length(LinkedList<int> myLList)
+    {
+        int count = 0;
+        for (int i = 0; i < myLList.Count; i++)
+        {
+            count += 1;
+        }
+        return(count);
+    }
+}
