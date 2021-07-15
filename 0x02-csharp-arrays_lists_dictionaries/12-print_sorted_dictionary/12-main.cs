269d50d8c7284340714d475e6d5f4e157010caf1
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 23:51:59 -0500

    my output looks right. what's going on

--- 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-main.cs
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-main.cs
@@ -7,10 +7,10 @@ class Program
     {
         Dictionary<string, string> myDict = new Dictionary<string, string>();
 
-        myDict.Add("language", "C");
-        myDict.Add("track", "low level");
+        myDict.Add("language", "python");
+        myDict.Add("track", "high level");
         myDict.Add("school", "Holberton");
-        myDict.Add("address", "972 Mission St.");
+        myDict.Add("address", "98 Battery St.");
         myDict.Add("city", "San Francisco");
 
         Dictionary.PrintSorted(myDict);
