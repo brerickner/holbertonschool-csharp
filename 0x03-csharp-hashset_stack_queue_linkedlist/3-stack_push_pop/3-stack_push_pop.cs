5c587a188b4d86247d77a3e4f39e1fe14384a280
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 15:10:23 -0500

    finally figured out the empty stack

--- 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -7,22 +7,30 @@ class MyStack
         Boolean condition = true;
 
         if (aStack == null || aStack.Count <= 0)
-            System.Console.WriteLine("Stack is empty");
-    
-        System.Console.WriteLine($"Number of items: {aStack.Count}");
-        System.Console.WriteLine($"Top item: {aStack.Peek()}");
-
-        if (aStack.Contains(search) == condition)
         {
-            System.Console.WriteLine($"Stack contains \"{search}\": {true}");
-            while (aStack.Contains(search) == condition && aStack.Count > 0)
+            System.Console.WriteLine("Stack is empty");
+            // aStack = new Stack<string>;
+        }
+        System.Console.WriteLine($"Number of items: {aStack.Count}");
+        if (aStack.Count > 0)
+            System.Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        if (search != null)
+        {
+            if (aStack.Contains(search) == condition)
             {
-                aStack.Pop();
+                System.Console.WriteLine($"Stack contains \"{search}\": {true}");
+                while (aStack.Contains(search) == condition && aStack.Count > 0)
+                {
+                    aStack.Pop();
+                }
             }
         }
         else
             System.Console.WriteLine($"Stack contains \"{search}\": {false}");
-        aStack.Push(newItem);
+
+        if(newItem != null)
+            aStack.Push(newItem);
         return (aStack);
     }
 }
