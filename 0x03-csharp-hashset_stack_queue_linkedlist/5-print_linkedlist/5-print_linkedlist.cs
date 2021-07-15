9553b422ba66d9db2a30bf499e4ea9521b20c699
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 17:31:04 -0500

    adding task 5. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> linkey = new LinkedList<int>();
+        if (size <= 0)
+        {
+            return(linkey);
+        }
+        for (int i = 0; i < size; i++)
+        {
+            System.Console.WriteLine(i);
+            linkey.AddLast(i);
+        }
+        return (linkey);
+    }
+}
