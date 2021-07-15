905b49ba22c22df055a2bd2d5d6a9a80811b063c
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 17:55:43 -0500

    adding task 7. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/7-linkedlist_add/7-linkedlist_add.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static LinkedListNode<int> Add(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> nodez = new LinkedListNode<int>(n);
+        myLList.AddFirst(nodez);
+        return (nodez);
+    }
+}
