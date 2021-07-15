0137e3a99deeac9d3c15493f7e532a2d91f5891d
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 23:49:47 -0500

    adding modified task 13

--- 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -4,8 +4,10 @@ class Dictionary
 {
     public static string BestScore(Dictionary<string, int> myList)
     {
-        string freshKey = "";
+        string freshKey = "None";
         int highScore = 0;
+        if (myList.Count == 0)
+            return ("None");
         foreach (KeyValuePair<string, int> newScore in myList)
         {
             if (newScore.Value > highScore)
