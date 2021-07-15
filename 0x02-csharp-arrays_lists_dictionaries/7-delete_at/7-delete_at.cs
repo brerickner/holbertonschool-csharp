c4d14f7c3dfd04be94962b255db2555b2288a925
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 20:39:03 -0500

    adding task 7. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (myList == null || myList.Count == 0)
+            return (null);
+
+        if (index < 0 || index > myList.Count - 1)
+        {
+            System.Console.WriteLine("Index is out of range");
+            return (myList);
+        }
+        myList.Remove(myList[index]);
+        return (myList);
+    }
+}
