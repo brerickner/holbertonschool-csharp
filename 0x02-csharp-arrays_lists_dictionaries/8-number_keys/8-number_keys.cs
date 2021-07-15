bb5e4c5702837bccfe1b52f1243d73aaf7fb4657
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 21:05:01 -0500

    adding task 8. output loooks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int keys_plz = 0;
+        foreach (var keys in myDict)
+        {
+            keys_plz++;
+        }
+        return(keys_plz);
+    }
+}
