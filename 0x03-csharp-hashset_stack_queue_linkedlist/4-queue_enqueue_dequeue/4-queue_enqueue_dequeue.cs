4690cfa71ccd9b2b6874582a48520ad7ed37247c
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 17:12:18 -0500

    adding task 4. output good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Boolean condition = true;
+
+        if (aQueue == null)
+        {
+            System.Console.WriteLine("Stack is empty");
+            // aQueue = new Stack<string>;
+        }
+        System.Console.WriteLine($"Number of items: {aQueue.Count}");
+        if (aQueue.Count > 0)
+            System.Console.WriteLine($"Top item: {aQueue.Peek()}");
+        else
+            System.Console.WriteLine("Stack is empty");
+
+        if (aQueue.Contains(search) == condition)
+        {
+            System.Console.WriteLine($"Stack contains \"{search}\": {true}");
+            while (aQueue.Contains(search) == condition && aQueue.Count > 0)
+            {
+                aQueue.Dequeue();
+            }
+        }
+        else
+            System.Console.WriteLine($"Stack contains \"{search}\": {false}");
+
+        if(newItem != null)
+            aQueue.Enqueue(newItem);
+        return (aQueue);
+    }
+}
+
