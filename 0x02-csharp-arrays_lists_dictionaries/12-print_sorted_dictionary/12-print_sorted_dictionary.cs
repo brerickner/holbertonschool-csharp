b8e3d784be2287923aa9e93353613fc355062a65
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-14 22:26:12 -0500

    adding task 12. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        var myList = myDict.Keys.ToList();
+        myList.Sort();
+        foreach (string keyz in myList)
+        {
+            System.Console.WriteLine("{0} : {1}", keyz, myDict[keyz]);
+        }
+    }
+}
