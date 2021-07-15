b8e80632516ddd094223652175992e586b974e2a
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 13:46:50 -0500

    adding task 3. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        Boolean condition = true;
+
+        if (aStack == null || aStack.Count <= 0)
+            System.Console.WriteLine("Stack is empty");
+    
+        System.Console.WriteLine($"Number of items: {aStack.Count}");
+        System.Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        if (aStack.Contains(search) == condition)
+        {
+            System.Console.WriteLine($"Stack contains {search}: {true}");
+            while (aStack.Contains(search) == condition && aStack.Count > 0)
+            {
+                aStack.Pop();
+            }
+        }
+        else
+            System.Console.WriteLine($"Stack contains {search}: {false}");
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
