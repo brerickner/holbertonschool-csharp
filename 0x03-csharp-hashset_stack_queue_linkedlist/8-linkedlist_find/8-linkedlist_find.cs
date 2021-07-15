2805b015aafd73bda9903cf065882bb8d7149f0e
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 18:15:28 -0500

    adding task 8. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int counter = 0;
+        for (var node = myLList.First; node != null; node = node.Next, counter++)
+        {
+            if (value.Equals(node.Value))
+                return (counter); 
+        }
+        return (-1);
+    }
+}
