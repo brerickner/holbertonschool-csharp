01586e9514458f36650364d0e6025283c488eb49
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 19:42:41 -0500

    adding task 12. output looks good

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        LinkedListNode<int> newNodez = myLList.First;
+    
+        for (;newNodez != null;newNodez = newNodez.Next)
+        {
+            if (newNodez.Value > n)
+            {
+                myLList.AddBefore(newNodez, n);
+                return (newNodez);
+            }
+        }
+        myLList.AddLast(n);
+        return(newNodez);
+    }
+}
