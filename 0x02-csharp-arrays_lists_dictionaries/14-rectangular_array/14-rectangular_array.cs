67d5b2417ef496d2b515e9bd9921642497a52a59
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-16 10:39:40 -0500

    adding task 14. output looks good

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int width = 5;
+            int height = 5;
+
+            int [,] arrayz = new int [width,height];
+            arrayz[2, 2] = 1;
+
+            for (int x = 0; x < width; x++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    System.Console.Write(arrayz[x,y]);
+                }
+                System.Console.WriteLine();
+            }
+        }
+    }
+}
