fdec9df5fadfd162a559cb810f607e90d5f0adcf
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-15 20:08:41 -0500

    added task

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> nodez = myLList.First;
+        int i = 0;
+        for (; i <= index && nodez != null;i++, nodez=nodez.Next)
+        {
+            if (index == i)
+            {
+                myLList.Remove(nodez);
+            }
+        }
+    }
+}
+
