ee00b328b2481bc28edf5f7e46ba59e03a6155e0
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-16 11:38:37 -0500

    adding task after PLD collab

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        
+        int row = myMatrix.GetLength(0);
+        int column = myMatrix.GetLength(1);
+        int[,] newMatrix = new int[row, column];
+
+        for (int x = 0; x < row; x++)
+        {
+            for (int y = 0; y < column; y++)
+            {
+                newMatrix[x, y] = myMatrix[x, y] * myMatrix[x, y];
+            }
+        }
+        return (newMatrix);
+    }
+}
