c0af4e15e19ddc27a94a49b8ef6f426503b29a03
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-26 10:26:19 -0500

    saving progress for task 0

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int index = 0;
+
+        try
+        {
+            for (;index <= n; index++)
+            {
+                System.Console.WriteLine(myList[index]);
+            }
+        }
+        catch (Exception)
+        {
+            return (index);
+        }
+        return (index);
+    }
+}
