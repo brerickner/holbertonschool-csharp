551041bf2c47a43800192257f2c37212d706312f
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-27 11:58:23 -0500

    adding task 3. Output looks good

---
+++ 0x06-csharp-classes/3-enemy/3-enemy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Represents a Zombie within Enemies</summary>
+    public class Zombie
+    {
+        /// <summary>Member <see cref="Zombie.health"/> of <see cref="Zombie"/> class.</summary>
+        public int health;
+        
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        /// <param name="value">Amount of health</param>
+        public Zombie(int value)
+        {
+            if (value < 0)
+                throw new ArgumentException("Health must be greater than or equal to 0");
+                health = value;
+        }
+        /// <summary>Retrieves health status of <see cref="Zombie"/> object.</summary>lth() 
+        /// <returns>that returns the value of health of the Zombie object</returns> 
+        public int GetHealth()
+        {
+            return (health);
+        }
+    }
+}
