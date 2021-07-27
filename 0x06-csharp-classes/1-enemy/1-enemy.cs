7178ab76856dfc9405b3097a7974d709f9444d4f
Bre <49533377+brerickner@users.noreply.github.com>
2021-07-27 11:24:00 -0500

    Adding task 1. Output looks good

---
+++ 0x06-csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>Represents a Zombie within Enemies</summary>
+    public class Zombie
+    {
+        /// <summary>Member <see cref="Zombie.health"/> of <see cref="Zombie"/> class.</summary>
+        public int health;
+        
+        /// <summary>Initializes a new instance of the <see cref="Zombie"/> class.</summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+    }
+}
