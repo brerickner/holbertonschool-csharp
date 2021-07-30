28c361cb1c87d47a2b3ce197f7f6815a40261055
brerickner <breannarickner@gmail.com>
2021-07-29 23:47:31 -0500

    adding 3 progress

--- 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/3-palindrome/Text.Tests/Text.Tests.cs
@@ -4,15 +4,23 @@ namespace Text.Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
-        public void Test1()
+        public void testPals()
         {
-            Assert.Pass();
+            string stringy = "helloworld";
+            string stringy2 = "";
+            string stringy3 = "meow";
+            string stringy4 = "kayak";
+            string stringy5 = "noon";
+            string stringy6 = "bob";
+
+
+            Assert.AreEqual(false, Str.IsPalindrome(stringy));
+            Assert.AreEqual(true, Str.IsPalindrome(stringy2));
+            Assert.AreEqual(false, Str.IsPalindrome(stringy3));
+            Assert.AreEqual(true, Str.IsPalindrome(stringy4));
+            Assert.AreEqual(true, Str.IsPalindrome(stringy5));
+            Assert.AreEqual(true, Str.IsPalindrome(stringy6));
         }
     }
 }
--- 0x07-csharp-tdd/3-palindrome/Text/Text.cs
+++ 0x07-csharp-tdd/3-palindrome/Text/Text.cs
@@ -2,7 +2,27 @@
 
 namespace Text
 {
-    public class Class1
+    /// <summary>
+    /// Str class
+    /// </summary>
+    public class Str
     {
+        /// <summary>
+        /// Gets true if string is palindrome
+        /// </summary>
+        /// <param name="s">The string</param>
+        /// <returns>returns True if a string is a palindrome or False if it’s not</returns>
+        public static bool IsPalindrome(string s)
+        {
+            string first = s.Substring(0, s.Length / 2);  
+            char[] arr   = s.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp   = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
     }
 }
