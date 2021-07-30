7c0b0960fb38876c41971923b221c11aa0f27db3
brerickner <breannarickner@gmail.com>
2021-07-29 23:31:13 -0500

    adding unique middle test

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -12,12 +12,15 @@ namespace Text.Tests
             string stringy3 = "meow";
             string stringy4 = "hheey";
             string stringy5 = "noon";
+            string stringy6 = "bob";
+
 
             Assert.AreEqual(0, Str.UniqueChar(stringy));
             Assert.AreEqual(-1, Str.UniqueChar(stringy2));
             Assert.AreEqual(0, Str.UniqueChar(stringy3));
             Assert.AreEqual(4, Str.UniqueChar(stringy4));
             Assert.AreEqual(-1, Str.UniqueChar(stringy5));
+            Assert.AreEqual(1, Str.UniqueChar(stringy6));
         }
     }
 }
