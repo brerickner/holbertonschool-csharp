d9502fa4dd3418fd1b8630165c4636755c2199c1
brerickner <breannarickner@gmail.com>
2021-07-29 23:28:49 -0500

    adding tests for 4

--- 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
+++ 0x07-csharp-tdd/4-unique/Text.Tests/Text.Tests.cs
@@ -8,7 +8,16 @@ namespace Text.Tests
         public void testUniqs()
         {
             string stringy = "helloworld";
+            string stringy2 = "";
+            string stringy3 = "meow";
+            string stringy4 = "hheey";
+            string stringy5 = "noon";
+
             Assert.AreEqual(0, Str.UniqueChar(stringy));
+            Assert.AreEqual(-1, Str.UniqueChar(stringy2));
+            Assert.AreEqual(0, Str.UniqueChar(stringy3));
+            Assert.AreEqual(4, Str.UniqueChar(stringy4));
+            Assert.AreEqual(-1, Str.UniqueChar(stringy5));
         }
     }
 }
