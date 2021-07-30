b4b8541e0b1e20ddf198aecab10aee31b6465c89
brerickner <breannarickner@gmail.com>
2021-07-29 22:58:45 -0500

    adding progress

--- 0x07-csharp-tdd/4-unique/Text/Text.cs
+++ 0x07-csharp-tdd/4-unique/Text/Text.cs
@@ -2,7 +2,29 @@
 
 namespace Text
 {
-    public class Class1
+    public class Str
     {
+        public static int UniqueChar(string s)
+        {
+            Dictionary<char,int>dict = new Dictionary<char,int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (!dict.ContainsKey(s[i]))
+                {
+                    dict.Add(s[i], 1); 
+                }
+                else
+                {
+                    dict[s[i]] +=1;
+                }
+            }
+            for (int i = 0; i <s.Length; i++)
+            {
+                if (dict[s[i]]==1)
+                {
+                    return i;
+                }
+            }
+            return -1;        
     }
 }
