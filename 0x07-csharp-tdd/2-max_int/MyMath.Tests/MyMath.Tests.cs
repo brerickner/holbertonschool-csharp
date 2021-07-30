2ab9dbf3261f6dc5d5c251a985cef32561c61965
brerickner <breannarickner@gmail.com>
2021-07-30 00:02:42 -0500

    ugh

---
+++ 0x07-csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,21 @@
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void testMax()
+        {
+            List<int> myNums = new List<int>{-5, 0, 1, -2};
+            // List<int> myNums = new List<int>{-5, 0, 1, -2};
+            // List<int> myNums = new List<int>{-5, 0, 1, -2};
+            // List<int> myNums = new List<int>{-5, 0, 1, -2};
+
+
+
+            Assert.AreEqual(1, Operations.Max(myNums));
+            // Assert.AreEqual(1, Operations.Max(myNums));
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MyMath
+{
+    public class Operations
+    {
+        public static int Max(List<int> nums)
+        {
+            var l = new List<int> (nums);
+            var max = l.max();
+            if (nums.Count == 0)
+            {
+                return (0);
+            }
+            else
+            {
+                return (max);
+            }
+        }
+    }
+}
