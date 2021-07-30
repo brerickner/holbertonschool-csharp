4fd02f6d618e1f003b23e31fbef74d6332278cf0
brerickner <breannarickner@gmail.com>
2021-07-29 22:31:43 -0500

    saving progress on 1. passing tests

--- 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -9,10 +9,14 @@ namespace MyMath.Tests
         public void CalculateALotDivisors()
         {
             int[,] testMatrix = new int[,]{{98, 0, -12}, {21, 972, 44}, {-727, 60, -2}};
+            int[,] testMatrix1 = new int[,]{{2, 2, 2}, {4, 6, 8}, {10, 12, 10}};
+
             int num = 0;
-            int[,] resultMatrix = Matrix.Divide(testMatrix, num);
-    
-            Assert.AreEqual(null, resultMatrix);
+            int num1 = 2;
+            
+            Assert.AreEqual(null, Matrix.Divide(testMatrix, num));
+            Assert.AreEqual(new int[,]{{1, 1, 1}, {2, 3, 4}, {5, 6, 5}}, Matrix.Divide(testMatrix1, num1));
+            Assert.IsNull(Matrix.Divide(null, num1));
         }
     }
 }
