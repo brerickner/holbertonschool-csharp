ae2b011ff8d9833e601e2d8f45b175f80fd2342d
brerickner <breannarickner@gmail.com>
2021-08-02 17:09:54 -0500

    adding task 2. output looks good

---
+++ 0x08-csharp-inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+    class Obj
+    {
+        /// <summary>
+        /// Checks if the object is an instance of a class that inherits from the
+        /// specified class. The object must be a subclass and doesn't apply when
+        /// object is an instance of the base class
+        /// </summary>
+        /// <param name="derivedType">The derived type</param>
+        /// <param name="baseType">The base type</param>
+        /// <returns>returns True if the object is an instance of a class that
+        /// inherits from the specified class, otherwise return False</returns>
+        public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+        {
+            return derivedType.IsSubclassOf(baseType)
+           && derivedType != baseType;
+        }
+    }
+
+
