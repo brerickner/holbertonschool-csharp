13061634e58cfbe656762fbe7b9cee92b272d652
brerickner <breannarickner@gmail.com>
2021-08-02 16:06:44 -0500

    adding setup script

---
+++ 0x08-csharp-inheritance/meow.sh
@@ -0,0 +1,13 @@
+# example usage: ./meow.sh 0-meow. Must still remove 
+if [ $# -eq 1 ];
+then
+    prog_name="$1".cs;
+    new='  <PropertyGroup>\n    <DocumentationFile>bin\\$(Configuration)\\$(TargetFramework)\\$(AssemblyName).xml</DocumentationFile>';
+    old='  <PropertyGroup>';
+    dotnet new console -o $1;
+    cd "$1";
+    mv Program.cs $prog_name;
+    sed -i "s@^$old@$new@" "$1".csproj;
+else
+    echo "Usage: ./mktest <file name>"
+fi
