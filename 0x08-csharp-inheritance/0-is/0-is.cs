64bfabe1eda3e0026709c4f7d1d10d95bb4deda7
brerickner <breannarickner@gmail.com>
2021-08-02 16:05:28 -0500

    adding 0

---
+++ 0x08-csharp-inheritance/0-is/0-is.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+    class Obj
+    {
+        /// <summary>
+        /// Check if object is an int
+        /// </summary>
+        /// <param name="obj">the object</param>
+        /// <returns>True if the object is an int, otherwise return False</returns>
+        public static bool IsOfTypeInt(object obj)
+        {
+            return obj.GetType() == typeof(int);
+        }
+    }
+
