c7903e1680ebebc8a6033f3363d503d4c7d20c0a
brerickner <breannarickner@gmail.com>
2021-08-02 19:27:16 -0500

    saving progress

---
+++ 0x08-csharp-inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+
+
+
+
+    class Obj
+    {
+        /// <summary>
+        /// Prints the names of the available properties and methods of an object
+        /// </summary>
+        /// <param name="myObj">the object</param>
+        public static void Print(object myObj)
+        {
+            TypeInfo theType = typeof(myObj).GetTypeInfo().DeclaredProperties;
+            TypeInfo meths = typeof(myObj).GetTypeInfo().DeclaredMethods;
+            
+            IEnumerable<PropertyInfo> pList = theList.DeclaredProperties;
+            IEnumerable<MethodInfo> mList = theList.DeclaredMethods;
+
+            IEnumerable<theType> p = new List<theType>();
+            IEnumerable<meths> m = new List<meths>();
+            // Get the properties of 'myObj' class object.
+            Console.WriteLine($"{myObj.GetType().Name} Properties:");
+            // System.Console.WriteLine(theType);
+
+            // System.Console.WriteLine(meths);
+            // foreach ()
+            // {
+                    // Console.WriteLine(myPropertyInfo[i].Name);
+            // }
+            // Console.WriteLine($"{myObj.GetType().Name} Methods:");
+            // for (int i = 0; i < myPropertyInfo.Length; i++)
+            // {
+            //     // Console.WriteLine(myPropertyInfo[i].Name);
+            // }
+
+        }
+
+    }
+
