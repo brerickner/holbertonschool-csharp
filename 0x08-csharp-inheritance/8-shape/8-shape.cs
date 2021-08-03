36429c7cefda0c8f4fb98f4942754f692dd3c795
brerickner <breannarickner@gmail.com>
2021-08-02 22:05:48 -0500

    adding task 8. output looks good

---
+++ 0x08-csharp-inheritance/8-shape/8-shape.cs
@@ -0,0 +1,103 @@
+﻿using System;
+
+/// <summary>
+/// Class Shape
+/// </summary>
+class Shape
+{
+    /// <summary>
+    /// Throw an exception for Not Implemented Exception
+    /// </summary>
+    /// <returns></returns>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
+/// <summary>
+/// Initializes a new instance of the <see cref="Shape"/> class.
+/// </summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+
+    /// <summary>
+    /// Retrieves width
+    /// </summary>
+    /// <value>width</value>
+    public int Width{
+        get => width;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+            {
+                width = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Retrieves height
+    /// </summary>
+    /// <value></value>
+    public int Height{
+        get => height;
+        set
+        {
+            if (value < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+            {
+                height = value;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Calculates area
+    /// </summary>
+    /// <returns></returns>
+    public new int Area()
+    {
+        return width * height;
+    }
+
+    /// <summary>
+    /// Overrides str repr
+    /// </summary>
+    /// <returns></returns>
+    public override string ToString()
+    {
+        return $"[Rectangle] {width} / {height}";
+    }
+}
+
+/// <summary>
+/// Inherited Square class from <see cref="Rectangle"/> class.
+/// </summary>
+class Square : Rectangle
+{
+    private int size;
+    
+    /// <summary>
+    /// Retrieves size
+    /// </summary>
+    /// <value>size</value>
+    public int Size
+    {
+        get => size;
+        set {
+            if (value < 0)
+                throw new ArgumentException("Size must be greater than or equal to 0");
+            else
+            {
+                size = this.Height = this.Width = value;
+            }
+        }
+    }
+}
+
