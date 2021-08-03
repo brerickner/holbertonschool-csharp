310a3841bf899001ff3c5d84befed8f8dbbae1b2
brerickner <breannarickner@gmail.com>
2021-08-02 20:42:04 -0500

    saving task 4. output looks good

---
+++ 0x08-csharp-inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+/// <summary>
+/// Class Animal
+/// </summary>
+class Animal 
+{}
+    /// <summary>
+    /// Class Dog inherited from Animal
+    /// </summary>
+    class Dog : Animal
+    {}
+
