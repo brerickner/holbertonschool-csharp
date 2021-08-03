8548d393422d1b1f6bd1c7d618c80c63b894b30e
brerickner <breannarickner@gmail.com>
2021-08-02 20:48:30 -0500

    adding task 5. output looks right I think depending on what the three dots are in their output

---
+++ 0x08-csharp-inheritance/5-shape/5-shape.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+    class Shape
+    {
+        /// <summary>
+        /// Throw an exception for Not Implemented Exception
+        /// </summary>
+        /// <returns></returns>
+        public virtual int Area()
+        {
+            throw new NotImplementedException("Area() is not implemented");
+        }
+    }
+
