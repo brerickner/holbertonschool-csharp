0ca80765444ef92e862d2cae33f124d90ef332a0
brerickner <breannarickner@gmail.com>
2021-08-23 13:43:27 -0500

    had the length incorrect for the 3d array

--- 0x09-csharp-linear_algebra/6-vector_addition/6-main.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-main.cs
@@ -4,14 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        double[] vector1 = { 1, -8 };
-        double[] vector2 = { -7, 0 };
+        double[] vector1 = { 9, 2, -12 };
+        double[] vector2 = { 22, -1, 0 };
         double[] result;
 
         result = VectorMath.Add(vector1, vector2);
 
-        Console.WriteLine("(" + result[0] + ", " + result[1] + ")");
-        // Console.WriteLine("(" + result[0] + ", " + result[1] + ", " + result[2] + ")");
+        // Console.WriteLine("(" + result[0] + ", " + result[1] + ")");
+        Console.WriteLine("(" + result[0] + ", " + result[1] + ", " + result[2] + ")");
         // Console.WriteLine("(" + result[0] + ")");
     }
 }
--- 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ 0x09-csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -21,11 +21,11 @@ class VectorMath
 
             return (result2);
         }
-        else if (vector1.GetLength(0) == 2 && vector2.GetLength(0) == 2)
+        else if (vector1.GetLength(0) == 3 && vector2.GetLength(0) == 3)
         {
-            result3[0] =vector1[0] + vector2[0];
-            result3[1] =vector1[1] + vector2[1];
-            result3[2] =vector1[2] + vector2[2];
+            result3[0] = vector1[0] + vector2[0];
+            result3[1] = vector1[1] + vector2[1];
+            result3[2] = vector1[2] + vector2[2];
 
             return (result3);
         }
