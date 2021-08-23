90dc039687bbf6bdddf55d607837032affb1de39
brerickner <breannarickner@gmail.com>
2021-08-23 12:43:58 -0500

    adding files for 31

---
+++ 0x09-csharp-linear_algebra/33-inverse_2D/33-inverse_2D.cs
@@ -0,0 +1,16 @@
+﻿using System;
+/// <summary>
+/// Class matrix math
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Get's inverse matrix of 2d matrix
+    /// </summary>
+    /// <param name="matrix">the matrix</param>
+    /// <returns>inversed matrix of -1 if not inversible or not 2d</returns>
+    public static double[,] Inverse2D(double[,] matrix)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
