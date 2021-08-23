8cabb91a3620693a9cf70145325cd77949af255e
brerickner <breannarickner@gmail.com>
2021-08-23 15:37:15 -0500

    adding 3

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+/// <summary>
+/// Vector math
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculates length of a vector
+    /// </summary>
+    /// <param name="vector">Vector</param>
+    /// <returns>Length of vector</returns>
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length == 2)
+        {
+            double result2D = 0;
+            result2D = vector[0] * vector[0];
+            result2D += (vector[1] * vector[1]);
+            return (Math.Round(Math.Sqrt(result2D), 2));
+
+        }
+        if (vector.Length == 3)
+        {
+            double[] result3D = 0;
+            result3D = vector[0] * vector[0];
+            result3D += (vector[1] * vector[1]);
+            result3D += (vector[2] * vector[2]);
+            return (Math.Round(Math.Sqrt(result3D), 3));
+        }
+        else
+        {
+            return (-1);
+        }
+    }
+}
---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-main.cs
@@ -0,0 +1,17 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] vector1 = { 7, -3, -9 };
+        double[] result;
+
+        result = VectorMath.Magnitude(vector);
+
+        // Console.WriteLine("(" + result[0] + ", " + result[1] + ")");
+        Console.WriteLine("should be: 11.79");
+        Console.WriteLine(t);
+        // Console.WriteLine("(" + result[0] + ")");
+    }
+}
