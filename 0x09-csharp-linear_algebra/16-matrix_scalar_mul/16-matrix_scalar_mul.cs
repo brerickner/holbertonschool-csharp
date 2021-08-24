29ab0793fc3c4080a0394d3007fa7086a6cd0f73
brerickner <breannarickner@gmail.com>
2021-08-24 11:23:21 -0500

    adding 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -14,14 +14,13 @@ class MatrixMath
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
         double[,] nope = {{ -1 }};
-        double[,] result = new double[matrix1.GetLength(0), matrix1.GetLength(1)];
+        double[,] result = new double[matrix.GetLength(0), matrix.GetLength(1)];
 
-        if ((matrix1.GetLength(0) == 3 && matrix2.GetLength(0) == 3 && matrix1.GetLength(1) == 3 && matrix2.GetLength(1) == 3) || 
-            (matrix1.GetLength(0) == 2 && matrix2.GetLength(0) == 2 && matrix1.GetLength(1) == 2 && matrix2.GetLength(1) == 2))
+        if (matrix.GetLength(0) == 3 && matrix.GetLength(1) == 3 || matrix.GetLength(0) == 2 && matrix.GetLength(1) == 2)
         {
-            for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int i = 0; i < matrix.GetLength(0); i++)
             {
-                for (int j = 0; j < matrix1.GetLength(1); j++)
+                for (int j = 0; j < matrix.GetLength(1); j++)
                 {
                     result[i, j] = (matrix[i, j] * scalar);
                 }
