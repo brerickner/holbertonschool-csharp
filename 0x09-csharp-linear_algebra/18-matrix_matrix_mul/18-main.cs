02dc0763930a10228f7de1375abe015618c75c6e
brerickner <breannarickner@gmail.com>
2021-08-24 13:55:57 -0500

    getting first element output correct finally

---
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[,] matrix1 = { { 2, 3 }, { -1, 0 } };
+        double[,] matrix2 = { { 1, 7 }, { -8, -2 } };
+        double[,] result;
+
+        result = MatrixMath.Multiply(matrix1, matrix2);
+
+        for (int i = 0; i < result.GetLength(0); i++)
+        {
+            for (int j = 0; j < result.GetLength(1); j++)
+            {
+                Console.Write(result[i, j]);
+                if (j != result.GetLength(1) - 1)
+                    Console.Write(", ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ 0x09-csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -13,21 +13,28 @@ class MatrixMath
     /// <returns>resulting matrix. Else -1</returns>
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2)
     {
-        /// The number of columns of the 1st matrix must equal the number of rows of the 2nd matrix.
-        /// And the result will have the same number of rows as the 1st matrix,
-        /// and the same number of columns as the 2nd matrix.
-        
         double[,] nope = {{ -1 }};
         double[,] result = new double[matrix1.GetLength(0), matrix2.GetLength(1)];
+        double hold;
 
         if (matrix1.GetLength(1) == matrix2.GetLength(0))
         {
-            for (int i = 0; i < matrix1.GetLength(0); i++)
+            for (int i = 0; i < matrix1.GetLength(0) - 1; i++)
             {
-                for (int j = 0; j < matrix2.GetLength(1); j++)
+                for (int j = 0; j < matrix2.GetLength(1) - 1; j++)
                 {
-                    result[i, j] = ((matrix1[i, j] * matrix2[i, j]) + (matrix1[i + 1, j] * matrix2[i, j + 1]));
+                    int flag = 0;
+                    hold = 0;
+
+                    while (flag < matrix1.GetLength(1) && flag < matrix2.GetLength(0))
+                    {
+                        hold += (matrix1[i, flag] * matrix2[flag, j]);
+                        flag++;
+                        
+                    }
+                    result[i, j] = hold;
                 }
+                
             }
             return result;
         }
@@ -35,7 +42,5 @@ class MatrixMath
         {
             return nope;
         }
-
-
     }
 }
