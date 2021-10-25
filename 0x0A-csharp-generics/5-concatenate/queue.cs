02fc922eb8cf4ce3d227f9ec52cc6da089a44b82
brerickner <breannarickner@gmail.com>
2021-10-25 16:55:51 -0500

    adding progress after task 5. Output matches example given

---
+++ 0x0A-csharp-generics/5-concatenate/queue.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+/// <summary>
+/// Class Queue.
+/// </summary>
+/// <typeparam name="T">Queue type</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Variable tail of Node type set to null
+    /// </summary>
+    public Node tail = null;
+    
+    /// <summary>
+    /// Head object of type Node
+    /// </summary>
+    public Node head = null;
+    
+    /// <summary>
+    /// Count type int
+    /// </summary>
+    public int count = 0;
+
+    /// <summary>
+    /// Check type of queue.
+    /// </summary>
+    /// <returns>Queue type.</returns>
+    public Type CheckType()
+    {
+        return(typeof(T));
+    }
+
+    /// <summary>
+    /// Class Node.
+    /// value: any type, set to null.
+    /// next: Node type, set to null.
+    /// constructor that takes value for new Node as only parameter.
+    /// </summary>
+    public class Node {
+        /// <summary>
+        /// Variable value of any type set to null.
+        /// </summary>
+        public T value;
+
+        /// <summary>
+        /// Variable next of Node type set to null
+        /// </summary>
+        public Node next = null;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="value">Node value.</param>
+        public Node(T value)
+        {
+            this.value = value;
+        }
+    }
+
+    /// <summary>
+    /// Method Enqueue to create new Node and add to end of queue.
+    /// If queue is empty, make new node the head of queue.
+    /// Count is incremented each time new Node is added
+    /// </summary>
+    public void Enqueue(T value)
+    {
+        Node newNode = new Node(value);
+        if (this.head == null)
+        {
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        else
+        {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.count++;
+    }
+        
+    /// <summary>
+    /// Method Count returns number of nodes in the Queue.
+    /// </summary>
+    /// <returns>Number of nodes in the Queue.</returns>
+    public int Count()
+    {
+        return(this.count);
+    }
+
+    /// <summary>
+    /// Method Dequeue removes the first Node in queue and returns its value.
+    /// If queue is empty, write 'Queue is empty' to console and return default
+    /// of parameters type.
+    /// </summary> 
+    public T Dequeue()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return(default(T));
+        }
+        else
+        {
+            Node temp = this.head;
+            this.head = this.head.next;
+            this.count--;
+            return(temp.value);
+        }
+    }
+
+    /// <summary>
+    /// Method Peek returns the value of the first node of the queue without removing the node.
+    /// If queue is empty write 'Queue is empty' to console and return
+    /// default value of parameters type.
+    /// </summary>
+    public T Peek()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return(default(T));
+        }
+        else
+        {
+            return(this.head.value);
+        }
+    }
+
+    /// <summary>
+    /// Method Print to print the queue starting from the head.
+    /// If the queue is empty write 'Queue is emtpy' to console.
+    /// </summary>
+    public void Print()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+        }
+        else
+        {
+            Node temp = this.head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.value);
+                temp = temp.next;
+            }
+        }
+    }
+
+    /// <summary>
+    /// Method Concatenate to concatenate all values in the queue only if the queue
+    /// is type String or Char. If queue is empty, print 'Queue is empty' and return null.
+    /// If queue is not type String or Char, print 'Concatenate() is for a queue
+    ///  of Strings or Chars only' to console and return null.
+    /// </summary>
+    public string Concatenate()
+    {
+        if (this.head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return(null);
+        }
+        else if (this.CheckType() == typeof(string) || this.CheckType() == typeof(char))
+        {
+            string result = "";
+            Node temp = this.head;
+            while (temp != null)
+            {
+                result += temp.value;
+                temp = temp.next;
+            }
+            return(result);
+        }
+        else
+        {
+            Console.WriteLine("Concatenate() is for a queue of Strings or Chars only");
+            return(null);
+        }
+    }
+}
