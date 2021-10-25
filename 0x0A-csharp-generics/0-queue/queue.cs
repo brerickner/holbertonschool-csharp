a69e41478a7744071cc2c7e31f69d537a6c17f9c
brerickner <breannarickner@gmail.com>
2021-10-25 12:21:40 -0500

    saving progress task 0

---
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// Class Queue.
+/// </summary>
+/// <typeparam name="T">Queue type</typeparam>
+class Queue<T>
+{
+    /// <summary>
+    /// Check type of queue.
+    /// </summary>
+    /// <returns>Queue type.</returns>
+    public Type CheckType()
+    {
+        return(typeof(T));
+    }
+}
+
