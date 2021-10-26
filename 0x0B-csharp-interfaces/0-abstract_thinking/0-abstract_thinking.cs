2bdb223ca80032fa3141e9a160670eb219e6a554
brerickner <breannarickner@gmail.com>
2021-10-26 11:10:34 -0500

    adding progress after task 0. Output matches example given

---
+++ 0x0B-csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+/// <summary>
+/// Abstract class Base 
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Public property name of type string
+    /// </summary>
+    public string name;
+    
+    /// <summary>
+    /// Override method ToString that returns string for name's type 'name is a type'
+    /// </summary>
+    /// <returns>string</returns>
+    public override string ToString()
+    {
+        return name + " is a " + this.GetType().Name;
+    }
+}
