855029db956454f778b41c282a1f396fd34dbc2d
brerickner <breannarickner@gmail.com>
2021-10-26 15:08:57 -0500

    adding progress for task 3. First line in output does not print name variable. Searching for fix to this bug

--- 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -72,18 +72,9 @@ public class Door : Base, IInteractive
     /// Public property name of type string
     /// </summary>
     public string name { get; set; }
-    
-    /// <summary>
-    /// Override method ToString that returns string for name's type 'name is a type'
-    /// </summary>
-    /// <returns>string</returns>
-    public override string ToString()
-    {
-        return name + " is a " + this.GetType().Name;
-    }
 
     /// <summary>
-    /// Constructor sets value of 'name' if provided, else default value of 'Door'
+    /// Constructor sets value of 'name' if provided, else defaults to value of 'Door'
     /// </summary>
     public Door(string name = "Door")
     {
@@ -110,76 +101,14 @@ public class Decoration : Base, IInteractive, IBreakable
     public string name { get; set; }
 
     /// <summary>
-    /// Public property durability of type int
+    /// Property durability of type int
     /// </summary>
     public int durability { get; set; }
-    
-    /// <summary>
-    /// Override method ToString that returns string for name's type 'name is a type'
-    /// </summary>
-    /// <returns>string</returns>
-    public override string ToString()
-    {
-        return name + " is a " + this.GetType().Name;
-    }
-
-    /// <summary>
-    /// Constructor sets value of 'name' if provided, else default value of 'Decoration'
-    /// </summary>
-    public Decoration(string name = "Decoration")
-    {
-        this.name = name;
-    }
-
-    /// <summary>
-    /// Method Interact implemented to print 'The name has been broken.' if
-    /// durability is 0 or less. 
-    /// Else if isQuestItem is true, print 'You look at the name. There's a key inside.'
-    /// Else isQuestItem is false, print 'You look at the 'name'. Not much to see here.'
-    /// </summary>
-    public void Interact()
-    {
-        if (durability <= 0)
-        {
-            Console.WriteLine("The " + this.name + " has been broken.");
-        }
-        else if (isQuestItem)
-        {
-            Console.WriteLine("You look at the " + this.name + ". There's a key inside.");
-        }
-        else
-        {
-            Console.WriteLine("You look at the " + this.name + ". Not much to see here.");
-        }
-    }
-
-    /// <summary>
-    /// Method Break implemented to decrement durability by 1.
-    /// If durability is greater than 0, print 'You hit the 'name'. It cracks.'
-    /// If durability is 0, print 'You smash the 'name'. What a mess.'
-    /// if durability is less than 0, print 'The 'name' is already broken.'
-    /// </summary>
-    public void Break()
-    {
-        if (durability > 0)
-        {
-            durability--;
-            Console.WriteLine("You hit the " + this.name + ". It cracks.");
-        }
-        else if (durability == 0)
-        {
-            Console.WriteLine("You smash the " + this.name + ". What a mess.");
-        }
-        else
-        {
-            Console.WriteLine("The " + this.name + " is already broken.");
-        }
-    }
 
     /// <summary>
     /// Public isQuestItem of type bool
     /// </summary>
-    public bool isQuestItem { get; set; }
+    public bool isQuestItem;
 
     /// <summary>
     /// Constructor sets value of name, durability and isQuestItem
@@ -190,13 +119,58 @@ public class Decoration : Base, IInteractive, IBreakable
     /// <exception cref="ArgumentException">'Durability must be greater than 0'</exception>
     public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
     {
-        if (durability <= 0)
+        if (durability < 1)
         {
             throw new ArgumentException("Durability must be greater than 0");
         }
         this.name = name;
         this.durability = durability;
         this.isQuestItem = isQuestItem;
+    }
+
+    /// <summary>
+    /// Method Interact.
+    /// If durability is 0 or less print 'The name has been broken.' 
+    /// Else if isQuestItem is true, print 'You look at the name. There's a key inside.'
+    /// Else isQuestItem is false, print 'You look at the 'name'. Not much to see here.'
+    /// </summary>
+    public void Interact()
+    {
+        if (durability < 1)
+        {
+            Console.WriteLine("The " + this.name + " has been broken.");
+        }
+        if (isQuestItem == true)
+        {
+            Console.WriteLine("You look at the " + this.name + ". There's a key inside.");
+        }
+        if (isQuestItem == false)
+        {
+            Console.WriteLine("You look at the " + this.name + ". Not much to see here.");
+        }
+    }
+
+    /// <summary>
+    /// Method Break.
+    /// If durability is greater than 0 print 'You hit the name. It cracks.'
+    /// If durability is 0, print 'You smash the name. What a mess.'
+    /// If durability is less than 0, print 'The name is already broken.'
+    /// </summary>
+    public void Break()
+    {
+        durability--;
+        if (durability > 0)
+        {
+            Console.WriteLine("You hit the " + this.name + ". It cracks.");
+        }
+        if (durability == 0)
+        {
+            Console.WriteLine("You smash the " + this.name + ". What a mess.");
+        }
+        else
+        {
+            Console.WriteLine("The " + this.name + " is already broken.");
+        }
     }
 
 }
