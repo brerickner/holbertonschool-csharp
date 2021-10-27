41de2848d083c31ed7b413cd13b7f298846310bb
brerickner <breannarickner@gmail.com>
2021-10-27 10:24:06 -0500

    adding progress for task 1. Output matches example given

---
+++ 0x0C-csharp-delegates_events/0-universal_health/0-universal_health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+/// <summary>Public class {Player}</summary>
+public class Player
+{
+    /// <summary>Public property type string {name}</summary>
+    private string name;
+
+    /// <summary>Public property type float {maxHp}</summary>
+    private float maxHp;
+
+    /// <summary>Public property type float {hp}</summary>
+    private float hp;
+
+    /// <summary>
+    /// Constructor.
+    /// Prototype: public Player(string name, float maxHp).
+    /// MaxHp must be greater than 0, else set maxHp to the default 100f 
+    /// and print 'maxHp must be greater than 0. maxHp set to 100f by default.'.
+    /// Value of hp set to maxHp.
+    /// </summary>
+    /// <param name="name">string name = Player(default)</param>
+    /// <param name="maxHp">float maxHp = 100f(default)</param>
+    public Player(string name = "Player", float maxHp = 100f)
+    {
+        if (maxHp <= 0)
+        {
+            Console.WriteLine("maxHp must be greater than 0. maxHp set to 100f by default.");
+            this.maxHp = 100f;
+        }
+        else
+        {
+            this.name = name;
+            this.maxHp = maxHp;
+        }
+        this.hp = maxHp;
+    }
+
+    /// <summary>
+    /// Public method type void {PrintHealth}.
+    /// Prints '{name} has {hp} / {maxHp} health'.
+    /// </summary>
+    public void PrintHealth()
+    {
+        Console.WriteLine("{0} has {1} / {2} health", this.name, this.hp, this.maxHp);
+    }
+}
