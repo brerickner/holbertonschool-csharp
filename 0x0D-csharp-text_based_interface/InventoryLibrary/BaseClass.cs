7d9e7e1f19ba84affcffd29c2e8760095507129d
brerickner <breannarickner@gmail.com>
2021-11-04 15:03:15 -0500

    implementing method to set properties of BaseClass

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -28,5 +28,21 @@ namespace InventoryLibrary
         /// Property {date_updated} of type {DateTime}
         /// </summary>
         public DateTime date_updated { get; set; }
+
+        /// <summary>
+        /// Method {BaseClass} to set the properties for BaseClass.
+        /// </summary>
+        /// <param name="id">{string}Globally Unique Identifier</param>
+        /// <param name="date_created">{DateTime}</param>
+        /// <param name="date_updated">{DateTime}</param>
+        public BaseClass(string id, DateTime date_created, DateTime date_updated)
+        {
+            Guid guid = Guid.NewGuid();
+
+            this.id = guid.ToString();
+            this.date_created = date_created;
+            this.date_updated = date_updated;
+        }
+
     }
 }
