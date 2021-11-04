140a2a33126621e69a49118f2cfa82ef59778341
brerickner <breannarickner@gmail.com>
2021-11-04 14:51:26 -0500

    adding task 0 along  with its unittests

--- 0x0D-csharp-text_based_interface/InventoryLibrary.Tests/InventoryLibrary.Tests.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary.Tests/InventoryLibrary.Tests.cs
@@ -1,18 +1,45 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using InventoryLibrary;
 
+/// <summary>
+/// Namespace InventoryLibrary.Tests
+/// </summary>
 namespace InventoryLibrary.Tests
 {
+    /// <summary>
+    /// Public class Tests
+    /// </summary>
     public class Tests
     {
         [SetUp]
+        /// <summary>
+        /// Public method {Setup}.
+        /// Set up objects for testing.
+        /// </summary>
         public void Setup()
         {
+            BaseClass meow = new BaseClass();
+            BaseClass meow1 = new BaseClass();
         }
 
         [Test]
-        public void Test1()
+        public void Test_BaseClass_Properties ()
         {
-            Assert.Pass();
+            meow.id = "007";
+            meow1.id = 3;
+            meow.date_updated = DateTime.Now;
+
+            Assert.Error(meow1.id);
+            Assert.IsNotNull(meow.id);
+            Assert.IsNotNull(meow.date_created);
+            Assert.IsNotNull(meow.date_updated);
+            Assert.AreEqual(meow.id, "007");
+            Assert.AreNotEqual(meow.date_created, meow.date_updated);
+            Assert.TypeOf(meow.date_created, typeof(DateTime));
+            Assert.TypeOf(meow.date_updated, typeof(DateTime));
+            Assert.TypeOf(meow.id, typeof(String));
         }
     }
 }
--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,8 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
+
+/// <summary>
+/// Name space {InventoryLibrary}.
+/// </summary>
 namespace InventoryLibrary
 {
-    public class Class1
+    /// <summary>
+    /// Public class {BaseClass} all classes will inherit from.
+    /// </summary>
+    public class BaseClass
     {
+        /// <summary>
+        /// Property {id} of type {string}
+        /// </summary>
+        public string id { get; set; }
+
+        /// <summary>
+        /// Property {date_created} of type {DateTime}
+        /// </summary>
+        public DateTime date_created { get; set; }
+
+        /// <summary>
+        /// Property {date_updated} of type {DateTime}
+        /// </summary>
+        public DateTime date_updated { get; set; }
     }
 }
