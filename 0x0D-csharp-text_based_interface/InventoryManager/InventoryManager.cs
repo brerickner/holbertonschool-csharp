4f5001b407ea3dcb57498beb41eed97b42b21222
brerickner <breannarickner@gmail.com>
2021-11-04 23:43:30 -0500

    Final pushes. All tests passing

---
+++ 0x0D-csharp-text_based_interface/InventoryManager/InventoryManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+using InventoryLibrary;
+using System.Collections.Generic;
+
+namespace InventoryManager
+{
+    class Program
+    {
+        /// <summary>
+        /// Console application.
+        /// On start, loads all objects from path '../storage/inventory_managemer.json'.
+        /// Prints an initial prompt of available commands and their usages.
+        /// 
+        /// Example command and usages:
+        ///     Inventory Manager
+        /// -------------------------
+        /// {ClassNames} show all ClassNames of objects
+        /// {All} show all objects
+        /// {All [ClassName]} show all objects of a ClassName
+        /// {Create [ClassName]} a new object
+        /// {Show [ClassName object_id]} an object
+        /// {Update [ClassName object_id]} an object
+        /// {Delete [ClassName object_id]} an object
+        /// {Exit}
+        ///     
+        /// </summary>
+        static void Main()
+        {
+            JSONStorage vault = new JSONStorage();
+           
+            while (!false)
+            {
+                string prompt= "Enter commands now :) ";
+                string usages = @"           Inventory Manager
+                                    -------------------------
+                            {ClassNames} show all ClassNames of objects
+                            {All} show all objects
+                            {All [ClassName]} show all objects of a ClassName
+                            {Create [ClassName]} a new object
+                            {Show [ClassName object_id]} an object
+                            {Update [ClassName object_id]} an object
+                            {Delete [ClassName object_id]} an object
+                            {Exit}";
+                Console.WriteLine(usages);
+                Console.WriteLine("");
+                Console.Write(prompt);
+                string command = Console.ReadLine();
+        
+                string[] command_split = command.Split(' ');
+                if (command_split.Length == 0)
+                {
+                    Console.WriteLine("Invalid command");
+                    continue;
+                }
+                if (command_split[0] == "ClassNames")
+                {
+                    System.Console.WriteLine("ClassNames: ");
+                }
+                if (command_split[0] == "Exit")
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
---
+++ 0x0D-csharp-text_based_interface/InventoryManager/Overflow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using InventoryLibrary;
+using System.Collections.Generic;
+
+    /// <summary>
+    /// Public class Overflow.
+    /// </summary>
+    public class Overflow
+    {
+        List<string> commands = new List<string>(){"ClassNames", "All", "Create", "Show", "Update","Delete", "Exit"};
+
+        /// <summary>
+        /// Public method GetAll.
+        /// </summary>
+        /// <param name="vault"></param>
+        public void GetAll(JSONStorage vault)
+        {
+            System.Console.WriteLine("Come back later for more commands!");
+        }
+        /// <summary>
+        /// Public method ClassNames.
+        /// </summary>
+        public void ClassNames(JSONStorage vault)
+        {
+            Console.WriteLine("ClassNames: ");
+            foreach (KeyValuePair<string, object> s in vault.objects)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
